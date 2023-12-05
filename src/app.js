ae04199cdc50f7a3df4be4a2d358f0f77da6ce05
hanchoel1 <74218333+hanchoel1@users.noreply.github.com>
2023-12-04 21:32:35 -0400

    agregue variables de entorno

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const { Pool } = require('pg');
 
+
+require('dotenv').config({ path: '../.env' });
+
 // Configuración de la base de datos
 const pool = new Pool({
-  user: 'postgres',
-  host: 'node_db',
-  database: 'test',
-  password: 'h',
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 const app = express();
@@ -133,5 +136,5 @@ app.delete('/directories/:id', async (req, res) => {
 
 // Iniciar el servidor
 app.listen(3000, () => {
-  console.log('Servidor escuchando en el puerto 3000');
+  console.log(`Servidor escuchando en el puerto ${process.env.DB_USER}`);
 });
